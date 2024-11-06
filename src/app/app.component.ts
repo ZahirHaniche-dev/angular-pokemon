53ab2ba316bd9007d13618207334bb71b61c3412
Haniche Zahir <zahir.haniche@inetum.com>
2024-11-06 13:16:02 +0100

    Interagir sur le cycle de vie dun composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Welcome to {{pokemons[1]}}! </h1>`,
+  template: `<h1> Welcome to {{ pokemonList[0] }}! </h1>`,
   styles: []
 })
-export class AppComponent {
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
 }
