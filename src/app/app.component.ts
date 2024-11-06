37fd89bb87a5603c0cca6b73abf60a8406253fe6
Haniche Zahir <zahir.haniche@inetum.com>
2024-11-06 11:55:42 +0100

    C'est quoi un Composant ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Welcome to {{title}}! </h1>`,
+  template: `<h1> Welcome to {{pokemons[1]}}! </h1>`,
   styles: []
 })
 export class AppComponent {
-  title = 'angular-pokemon';
+  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
 }
