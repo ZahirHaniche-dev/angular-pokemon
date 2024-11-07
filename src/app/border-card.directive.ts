39aa17712a6f900147150147b85a31b7e2a180ae
Haniche Zahir <zahir.haniche@inetum.com>
2024-11-07 11:24:54 +0100

    Améliorer notre directive dattribut

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -5,19 +5,23 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 220;
+
   constructor(private el: ElementRef) { 
-    this.setHeight(220);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
 
   @Input('pkmnBorderCard') borderColor: string; // alias
-  @Input() pkmnBorderCard: string; // alias
+  @Input() pkmnBorderCard: string; // sans alias
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   setHeight(height: number) {
